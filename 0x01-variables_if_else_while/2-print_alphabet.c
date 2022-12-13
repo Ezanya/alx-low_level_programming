6374a69ad4f97e16b4b10960a0ec672fe2e670f1
Ezanya <findezanya@yahoo.com>
2022-12-13 14:13:41 -0800

    sdfa

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints lowercase alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char a[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(a[i]);
+	}
+	putchar('\n');
+	return (0);
+}
