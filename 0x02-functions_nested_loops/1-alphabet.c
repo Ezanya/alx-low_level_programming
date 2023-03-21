473011bfd2155326df99b59d2f0136bc52bc923b
Ezanya <findezanya@yahoo.com>
2023-03-21 13:40:11 -0700

    afa

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Print _putchar
+ * Return: Always 0
+ */
+int main(void)
+{       
+        int x = 97;
+        while (x < 123){
+                _putchar(x);
+                x++;
+        }
+        _putchar('\n');
+        return(0);
+} 
