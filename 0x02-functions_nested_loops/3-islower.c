e4085107300da480477934e562e8847e14b29f96
Ezanya <findezanya@yahoo.com>
2023-03-23 02:18:58 -0700

    asdgdsga

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
  * _islower - check if the character is lower case
- * return - 0 for capital and 1 for lower case
+ * Return: 0 for capital and 1 for lower case
+ * @c: char type letter
  */
 
 int _islower(int c)
