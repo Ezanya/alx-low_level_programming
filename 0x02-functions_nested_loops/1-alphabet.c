720239b4df5458e411db23f08d570e9906d87cc6
Ezanya <findezanya@yahoo.com>
2023-03-23 10:31:01 +0300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
 * main - entry point
-* return: always 0 
+* return: always 0
 */
 void print_alphabet(void)
 {
         int x = 97;
-        while (x < 123){
+        while (x < 123)
+        {
                 _putchar(x);
                 x++;
         }
